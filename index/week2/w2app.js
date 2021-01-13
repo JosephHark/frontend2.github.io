9c006dd08cc41772579484c2d839c94404591aa2
JosephHark <71291466+JosephHark@users.noreply.github.com>
2021-01-13 11:33:23 -0500

    c

--- index/week2/w2app.js
+++ index/week2/w2app.js
@@ -1,3 +1,29 @@
 const question = "What is Superman's real name?"
 const answer = prompt(question);
 alert(`You answered ${answer}`);
+if(answer == "Clark Kent"){
+    alert("You got it right! Clark Kent is the secert identity of Superman.")
+} else if(answer == "clark kent"){
+    alert("You got it right! Clark Kent is the secert identity of Superman.")
+
+} else {
+    alert("I'm sorry that asnwer is incorrect. Clark Kent is the secert identity of Superman.")
+}
+
+const quiz = [
+    ["What is Superman's real name?","Clark Kent"],
+    ["What is Wonder Woman's real name?","Diana Prince"],
+    ["What is Batman's real name?","Bruce Wayne"]
+];
+let score = 0 // initialize score
+for(const [question,answer] of quiz){
+    const response = prompt(question);
+    if(response === answer){
+        alert('Correct!');
+        score++;
+    } else {
+        alert(`Wrong! The correct answer was ${answer}`);
+    }
+}
+// At the end of the game, report the player's score
+alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
