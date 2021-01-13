a2da8a163b19368632a356b910d3132a99bc94db
JosephHark <71291466+JosephHark@users.noreply.github.com>
2021-01-12 21:20:32 -0500

    c

--- js/adding.js
+++ js/adding.js
@@ -1,17 +1,37 @@
-function addfunction() {
+function reqOne() {
+    const input = document.getElementById('req1').value;
+    const outputElement = document.getElementById('output');
+
+    outputElement.innerHTML = 'You entered: ' + input;
+};
+
+function reqTwo() {
     const input = document.getElementById('number').value;
     const outputElement = document.getElementById('total');
 
     const inputInt = parseInt(input);
     if (inputInt !== NaN) {
-      outputElement.innerHTML = 'Total: ' + sum(inputInt);
+        outputElement.innerHTML = 'Total: ' + sum(inputInt);
     }
-  }
-  
-  function sum(number) {
+}
+
+function sum(number) {
     let total = 0;
     for (let i = 1; i <= number; i++) {
-      total += i;
+        total += i;
     }
     return total;
-  }
+}
+
+function reqThree() {
+    const input1 = document.getElementById('number1').value;
+    const input2 = document.getElementById('number2').value;
+    const outputElement = document.getElementById('total');
+
+    const num1Float = parseFloat(input1);
+    const num2Float = parseFloat(input2);
+    if ((num1Float !== NaN) & (num2Float !== NaN)) {
+        const total = num1Float + num2Float;
+        outputElement.innerHTML = 'Sum of the numbers entered: ' + total;
+    }
+}
