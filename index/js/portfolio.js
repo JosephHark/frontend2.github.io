9d8542f511cc128e51c698c6b10e4e1f74c195a4
JosephHark <71291466+JosephHark@users.noreply.github.com>
2021-01-13 09:47:56 -0500

    c

--- index/js/portfolio.js
+++ index/js/portfolio.js
@@ -23,3 +23,21 @@ li.appendChild(a);
 
 ol.appendChild(li);
 }
+
+const team = [{
+    label: "Week 02 Team Activity",
+    url: "teamActivity/team02.html"
+}
+]
+
+const ol = document.querySelector("ol");
+
+for (const item of links) {
+const li = document.createElement("li");
+const a = document.createElement("a");
+a.setAttribute("href", item.url);
+a.textContent = item.label;
+li.appendChild(a);
+
+ol.appendChild(li);
+}
