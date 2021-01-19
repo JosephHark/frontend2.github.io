016cf50ca5a6dc13690f2016ce35ccd1b75589b5
JosephHark <71291466+JosephHark@users.noreply.github.com>
2021-01-19 12:29:45 -0500

    c

--- index/js/portfolio.js
+++ index/js/portfolio.js
@@ -1,52 +1,56 @@
 const links = [{
-    label: "Week 01 Notes",
-    url: "week1/notes.html"
-},
-{
-    label: "Week 02 Notes",
-    url: "week2/notes.html"
-},
-{
-    label: "Week 02 Application",
-    url: "week2/app.html"
-},
+        label: "Week 01 Notes",
+        url: "week1/notes.html"
+    },
+    {
+        label: "Week 02 Notes",
+        url: "week2/notes.html"
+    },
+    {
+        label: "Week 02 Application",
+        url: "week2/app.html"
+    },
 
-{
-    label: "Week 03 Notes",
-    url: "week3/notes.html"
-},
-{
-    label: "Week 03 Application",
-    url: "week3/app.html"
-}
+    {
+        label: "Week 03 Notes",
+        url: "week3/notes.html"
+    },
+    {
+        label: "Week 03 Application",
+        url: "week3/app.html"
+    },
+
+    {
+        label: "Week 03 Application",
+        url: "week3/heroes.html"
+    }
 ]
 
 const ol = document.getElementById("myWork")
 
 for (const item of links) {
-const li = document.createElement("li");
-const a = document.createElement("a");
-a.setAttribute("href", item.url);
-a.textContent = item.label;
-li.appendChild(a);
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.setAttribute("href", item.url);
+    a.textContent = item.label;
+    li.appendChild(a);
 
-ol.appendChild(li);
+    ol.appendChild(li);
 }
 
 const team = [{
     label: "Week 02 Team Activity",
     url: "teamActivities/team02.html"
-}
-]
+}]
 
 const teams = document.getElementById("teams")
 
 for (const item of team) {
-const li = document.createElement("li");
-const a = document.createElement("a");
-a.setAttribute("href", item.url);
-a.textContent = item.label;
-li.appendChild(a);
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.setAttribute("href", item.url);
+    a.textContent = item.label;
+    li.appendChild(a);
 
-teams.appendChild(li);
+    teams.appendChild(li);
 }
---
+++ index/week3/js/heroes.js
@@ -0,0 +1,10 @@
+const flash = "Flash";
+const aquaman = "Aquaman";
+const heroes = getElementById("rooster");
+
+heroes.appendChild(flash);
+heroes.insertBefore(aquaman,wonderWoman);
+const h1 = document.getElementById('title');
+const oldText = h1.firstChild;
+const newText = document.createTextNode('Justice League of America');
+h1.replaceChild(newText,oldText);
