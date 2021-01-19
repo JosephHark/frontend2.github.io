321635d2a1799a56c3a6e87faa6d92d3433fff72
JosephHark <71291466+JosephHark@users.noreply.github.com>
2021-01-19 12:43:15 -0500

    c

--- index/week3/js/w3app.js
+++ index/week3/js/w3app.js
@@ -10,34 +10,56 @@ const quiz = [
     { name: "Shazam",realName: "Billy Batson" },
     
 ];
-const game = {
+
+// View Object
+const view = {
+    score: document.querySelector('#score strong'),
+    question: document.getElementById('question'),
+    result: document.getElementById('result'),
+    info: document.getElementById('info'),
+    render(target,content,attributes) {
+        for(const key in attributes) {
+          target.setAttribute(key, attributes[key]);
+        }
+        target.innerHTML = content;
+    }
+  };
+  
+  // Game Object
+  const game = {
     start(quiz){
-        this.questions = [...quiz];
-        this.score = 0;
-        // main game loop
-        for(const question of this.questions){
+      this.score = 0;
+      this.questions = [...quiz];
+      // main game loop
+      for(const question of this.questions){
         this.question = question;
         this.ask();
-        }
-        // end of main game loop
-        this.gameOver();
+      }
+      // end of main game loop
+      this.gameOver();
     },
     ask(){
-        const question = `What is ${this.question.name}'s real name?`;
-        const response =  prompt(question);
-        this.check(response);
+      const question = `What is ${this.question.name}'s real name?`;
+      view.render(view.question,question);
+      const response =  prompt(question);
+      this.check(response);
     },
     check(response){
-        const answer = this.question.realName;
-        if(response === answer){
+      const answer = this.question.realName;
+      if(response === answer){
+        view.render(view.result,'Correct!',{'class':'correct'});
         alert('Correct!');
         this.score++;
-        } else {
+        view.render(view.score,this.score);
+      } else {
+        view.render(view.result,`Wrong! The correct answer was ${answer}`,{'class':'wrong'});
         alert(`Wrong! The correct answer was ${answer}`);
-        }
+      }
     },
     gameOver(){
-        alert(`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
+      view.render(view.info,`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
     }
-}
-game.start(quiz);
+  }
+  
+  game.start(quiz);
+  
