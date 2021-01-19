96024d32f01e44966ab75294d112206b2aff8fee
JosephHark <71291466+JosephHark@users.noreply.github.com>
2021-01-18 19:23:13 -0500

    x

--- index/week3/js/w3app.js
+++ index/week3/js/w3app.js
@@ -2,4 +2,42 @@ const quiz = [
     { name: "Superman",realName: "Clark Kent" },
     { name: "Wonder Woman",realName: "Diana Prince" },
     { name: "Batman",realName: "Bruce Wayne" },
+    { name: "Supergirl",realName: "Kara Danvers" },
+    { name: "Green Lantern",realName: "Hal Jordan" },
+    { name: "Aquaman",realName: "Authur Curry" },
+    { name: "Flash",realName: "Barry Allen" },
+    { name: "Martian Manhunter",realName: "Johnn Jonzz" },
+    { name: "Shazam",realName: "Billy Batson" },
+    
 ];
+const game = {
+    start(quiz){
+        this.questions = [...quiz];
+        this.score = 0;
+        // main game loop
+        for(const question of this.questions){
+        this.question = question;
+        this.ask();
+        }
+        // end of main game loop
+        this.gameOver();
+    },
+    ask(){
+        const question = `What is ${this.question.name}'s real name?`;
+        const response =  prompt(question);
+        this.check(response);
+    },
+    check(response){
+        const answer = this.question.realName;
+        if(response === answer){
+        alert('Correct!');
+        this.score++;
+        } else {
+        alert(`Wrong! The correct answer was ${answer}`);
+        }
+    },
+    gameOver(){
+        alert(`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
+    }
+}
+game.start(quiz);
