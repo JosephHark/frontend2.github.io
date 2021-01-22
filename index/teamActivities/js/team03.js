e7aac3b64d8cc498a008acd2f18ede75b130d53b
JosephHark <71291466+JosephHark@users.noreply.github.com>
2021-01-21 20:57:55 -0500

    c

--- index/teamActivities/js/team03.js
+++ index/teamActivities/js/team03.js
@@ -1,4 +1,3 @@
-const old = inventors.filter(function (year) {
-  return (year < 1499) && (year > 1600);
-});
+const old = inventors.filter(inventor => (inventor.year >= 1500 && inventor.year < 1600));
+
 console.log(old);
