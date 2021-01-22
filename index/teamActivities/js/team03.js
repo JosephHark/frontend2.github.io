cf1df12a365c8126259df3414549934af848201d
JosephHark <71291466+JosephHark@users.noreply.github.com>
2021-01-21 21:35:53 -0500

    v

--- index/teamActivities/js/team03.js
+++ index/teamActivities/js/team03.js
@@ -1,3 +1,20 @@
 const old = inventors.filter(inventor => (inventor.year >= 1500 && inventor.year < 1600));
-
 console.log(old);
+
+const fullNames = inventors.map(inventor => `${inventor.first} ${inventor.last}`);
+console.log(fullNames);
+
+const sorted = inventors.sort((a, b) => a.year > b.year ? 1 : -1)
+console.log(sorted);
+
+const totalYears = inventors.reduce((total, inventor) => {
+  return total + (inventor.passed - inventor.year);
+},0);
+console.log(totalYears);
+
+const oldest = inventors.sort(function(a, b) {
+  const lastInventor = a.passed - a.year;
+  const nextInventor = b.passed - b.year;
+  return lastInventor > nextInventor ? -1 : 1;
+});
+console.log(oldest);
