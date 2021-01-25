f47ae8fe8dfdfb0e21a010835c5369040cadc173
JosephHark <71291466+JosephHark@users.noreply.github.com>
2021-01-25 17:39:15 -0500

    c

---
+++ index/week4/js/search.js
@@ -0,0 +1,23 @@
+input.addEventListener('blur', () => alert('blurred'), false);
+
+input.addEventListener('change', () => alert('changed'), false);
+const form = document.forms['search'];
+form.addEventListener ('submit', search, false);
+
+function search(event) {
+    alert(`You Searched for: ${input.value}`);
+    alert('Form Submitted');
+    event.preventDefault();
+}
+
+input.value = 'Search Here';
+
+input.addEventListener('focus', function(){
+    if (input.value==='Search Here') {
+        input.value = '' 
+    }
+}, false);
+input.addEventListener('blur', function(){
+    if(input.value === '') {
+        input.value = 'Search Here';
+    } }, false);
