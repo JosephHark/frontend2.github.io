8fbfb4f159f87542392c98c332a6b5d7ea53ce3c
JosephHark <71291466+JosephHark@users.noreply.github.com>
2021-01-28 19:12:22 -0500

    c

--- index/teamActivities/js/team04.js
+++ index/teamActivities/js/team04.js
@@ -1,3 +1,27 @@
-function myFunction() {
+function one() {
     document.getElementById("one").innerHTML = "Hello World";
-  }
+}
+function two() {
+    document.getElementById("two").innerHTML = "Hello World";
+}
+function three() {
+    document.getElementById("three").innerHTML = "Hello World";
+}
+function four() {
+    document.getElementById("four").innerHTML = "Hello World";
+}
+function five() {
+    document.getElementById("five").innerHTML = "Hello World";
+}
+function six() {
+    document.getElementById("six").innerHTML = "Hello World";
+}
+function seven() {
+    document.getElementById("seven").innerHTML = "Hello World";
+}
+function eight() {
+    document.getElementById("eight").innerHTML = "Hello World";
+}
+function nine() {
+    document.getElementById("nine").innerHTML = "Hello World";
+}
