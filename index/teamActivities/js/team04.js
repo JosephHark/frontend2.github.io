14f10d2e1ae43f4a26c3d5862f2ad1b4128e110c
JosephHark <71291466+JosephHark@users.noreply.github.com>
2021-01-29 11:37:21 -0500

    c

--- index/teamActivities/js/team04.js
+++ index/teamActivities/js/team04.js
@@ -1,27 +1,25 @@
-function one() {
-    document.getElementById("one").innerHTML = "Hello World";
+const board = document.getElementById('board');
+const resetButton = document.getElementById('reset');
+const player1 = 'X';
+const player2 = 'O';
+let player = player1;
+
+function addPiece(e) {
+    console.log(e.target);
+    e.target.innerHTML = player;
+    if (player === player1) player = player2;
+    else player = player1;
 }
-function two() {
-    document.getElementById("two").innerHTML = "Hello World";
+// table version
+function resetBoard() {
+    console.dir(board);
+    for (let i = 0; i < board.rows.length; i++) {
+        let row = board.rows[i];
+        for (let j = 0; j < row.cells.length; j++) {
+            row.cells[j].innerHTML = '';
+        }
+    }
 }
-function three() {
-    document.getElementById("three").innerHTML = "Hello World";
-}
-function four() {
-    document.getElementById("four").innerHTML = "Hello World";
-}
-function five() {
-    document.getElementById("five").innerHTML = "Hello World";
-}
-function six() {
-    document.getElementById("six").innerHTML = "Hello World";
-}
-function seven() {
-    document.getElementById("seven").innerHTML = "Hello World";
-}
-function eight() {
-    document.getElementById("eight").innerHTML = "Hello World";
-}
-function nine() {
-    document.getElementById("nine").innerHTML = "Hello World";
-}
+
+board.addEventListener('click', addPiece);
+reset.addEventListener('click', resetBoard);
