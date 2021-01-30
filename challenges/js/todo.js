c9a9e94f0276701c45581ac1bd474a7d4c194c3a
JosephHark <71291466+JosephHark@users.noreply.github.com>
2021-01-30 12:30:54 -0500

    c

---
+++ challenges/js/todo.js
@@ -0,0 +1,7 @@
+const list = document.getElementById('todoList');
+const myToDos = new ToDoController(list);
+
+document.getElementById('addToDo').addEventListener('click', () => {
+  const task = document.getElementById('todoInput').value;
+  myToDos.newToDo(task);
+});
