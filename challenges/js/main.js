0732f386b2c60faa3024f8c231265ae58c41ddee
JosephHark <71291466+JosephHark@users.noreply.github.com>
2021-02-08 17:59:40 -0500

    c

--- challenges/js/main.js
+++ challenges/js/main.js
@@ -7,4 +7,58 @@ document.addEventListener("readystatechange", (event) =>{
     if (event.target.readyState ==="complete"){
         initApp();
     }
-})
+});
+
+const initApp = () =>{
+    // Add listeners
+
+    //Procedural
+    //load list
+    //refresh page
+    refreshPage();
+};
+
+const refreshPage= () =>{
+    clearList();
+    renderList();
+    clearItemEntryfeild();
+    setFocusonItems();
+};
+
+const clearList = () =>{
+    const parentElement = document.getElementById("listItems");
+    deleteContents(parentElement);
+};
+
+const deleteContents = (parentElement) =>{
+    let child = parentElement.lastElementChild;
+    while (child){
+        parentElement.removeChild(child);
+        child = parentElement.lastElementChild;
+    }
+};
+
+const renderList = () =>{
+    const list= toDoList.getList();
+    list.forEach((item) =>{
+        buildListItem(item);
+    });
+};
+
+const buildListItem = (item) =>{
+    const div = document.createElement("div");
+    div.className = "item";
+    const check = document.createElement("input");
+    check.type = "checkbox";
+    check.id = item.getId();
+    check.tabIndex = 0;
+     addClickListener(check);
+    const container = document.getElementById("storeList");
+    container.appendChild(div);
+};
+
+const addClickListener = (checkbox) =>{
+    checkbox.addClickListener("click", (event)=>{
+        toDoList.removeItemFromList(checkbox.id)
+    
+});
--- challenges/js/todoList.js
+++
@@ -1,6 +0,0 @@
-import Todo from './todolists.js';
-//on load grab the array and insert it into the page
-const myToDoList = new items('items');
-window.addEventListener('load', () => {
-    myToDoList.showToDoList();
-}); 
