6b9ca8114849668549ce53ccc0cb1f2a261aaca9
JosephHark <71291466+JosephHark@users.noreply.github.com>
2021-02-11 13:28:42 -0500

    c

--- challenges/js/main.js
+++ challenges/js/main.js
@@ -5,42 +5,53 @@ const toDoList = new ToDoList();
 
 document.addEventListener("readystatechange", (event) => {
     if (event.target.readyState === "complete") {
-        initApp();
+        inItApp();
     }
 });
 
-const initApp = () => {
+const inItApp = () => {
     // Add listeners
-    const storeList = document.getElementById("storeList");
-    storeList.addEventListener("submit", (event) => {
+    const itemEntryForm = document.getElementById("itemEntryForm");
+    itemEntryForm.addEventListener("submit", (event) => {
         event.preventDefault();
         processSubmission();
     });
 
+    const clearItems = document.getElementById("clearItems");
+    clearItems.addEventListener("click", (event) => {
+        if (list.length) {
+            const confirmed = confirm("Are you sure that you want to clear the entire list?");
+            if (confirmed) {
+                toDoList.clearList();
+
+                refreshThePage();
+            };
+        };
+    });
     //Procedural
     loadListObj();
-    refreshPage();
+    refreshThePage();
 };
 
-const loadListObj = () =>{
+const loadListObj = () => {
     const storedList = localStorage.getItem("myToDoList");
     if (typeof storedList !== "string") return;
-    const parsedList =JSON.parse(storedList);
-    parsedList.forEach(itemObj =>{
+    const parsedList = JSON.parse(storedList);
+    parsedList.forEach(itemObj => {
         const newToDoItem = createNewItem(itemObj._id, itemObj._item);
         toDoList.AddItemToList(newToDoItem);
     });
 };
 
-const refreshPage = () => {
-    clearList();
+const refreshThePage = () => {
+    clearListDisplay();
     renderList();
     clearItemEntryfield();
     setFocusonItems();
 };
 
-const clearList = () => {
-    const parentElement = document.getElementById("newItem");
+const clearListDisplay = () => {
+    const parentElement = document.getElementById("listItems");
     deleteContents(parentElement);
 };
 
@@ -62,26 +73,30 @@ const renderList = () => {
 const buildListItem = (item) => {
     const div = document.createElement("div");
     div.className = "item";
+
     const check = document.createElement("input");
     check.type = "checkbox";
     check.id = item.getId();
     check.tabIndex = 0;
     addClickListener(check);
-    const container = document.getElementById("storeList");
+
+    const label = document.createElement("label");
+    label.htmlFor = item.getId();
+    label.textContent = item.getId();
+    div.appendChild(line);
+    div.appendChild(check);
+    div.appendChild(label);
+    const container = document.getElementById("listItems");
     container.appendChild(div);
 };
 
 const addClickListener = (checkbox) => {
     checkbox.addClickListener("click", (event) => {
-        toDoList.removeItemFromList(checkbox.id)
-        updatePersistentDate(toDoList.getList());
-        setTimeout(() => {
-            refreshPage();
-        }, 5000);
-    })
+        element.classList.add("completed")
+    });
 };
 
-const updatePersistentDate = (listArray) =>{
+const updatePersistentDate = (listArray) => {
     localStorage.setItem("myToDoList", JSON.stringify(listArray));
 };
 
@@ -95,24 +110,26 @@ const setFocusonItems = () => {
 
 const processSubmission = () => {
     const newEntryText = getNewEntry();
-    if (getNewEntry.length) return;
+    if (!newEntryText.length) return;
     const nextItemId = calcNextItemId();
     const toDoItem = createNewItem(nextItemId, newEntryText);
     toDoList.AddItemToList(toDoItem);
     updatePersistentDate(toDoList.getList());
-    refreshPage();
-
+    refreshThePage();
 };
 
 const getNewEntry = () => {
-    return document.getElementById("newItem").value.trim();
+    const text = document.getElementById("newItem").value.trim();
+    const date = document.getElementById("newItemDate").value.trim();
+    text.appendChild(date);
+    return text;
 };
 
 const calcNextItemId = () => {
     let nexItemId = 1;
     const list = toDoList.getList();
     if (list.length > 0) {
-        nexItemId = list(list.length - 1).getId() + 1;
+        nexItemId = list[list.length - 1].getId() + 1;
     }
     return nexItemId;
 };
