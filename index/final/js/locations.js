b5ba8d01d3b2f2c8e630b9d5146ca2a73b928c9b
JosephHark <71291466+JosephHark@users.noreply.github.com>
2021-03-02 16:41:34 -0500

    c

--- index/final/js/locations.js
+++ index/final/js/locations.js
@@ -32,68 +32,68 @@ const locationList = [{
 
 const imgBasePath = '//byui-cit.github.io/cit261/examples/';
 
-export default class Locations {
+export default class Hikes {
 constructor(elementId) {
   this.parentElement = document.getElementById(elementId);
   // we need a back button to return back to the list. This will build it and hide it. When we need it we just need to remove the 'hidden' class
   this.backButton = this.buildBackButton();
 }
 // why is this function necessary?  locationList is not exported, and so it cannot be seen outside of this module. I added this in case I ever need the list of locations outside of the module. This also sets me up nicely if my data were to move. I can just change this method to the new source and everything will still work if I only access the data through this getter.
-getAllLocations() {
+getAllHikes() {
   return locationList;
 }
 // For the first stretch we will need to get just one location.
-getLocationByName(locationName) {
-  return this.getAllLocations().find(location => location.name === locationName);
+getHikeByName(locationName) {
+  return this.getAllHikes().find(location => location.name === locationName);
 }
 //show a list of locations in the parentElement
-showLocationList() {
+showHikeList() {
   this.parentElement.innerHTML = '';
   // notice that we use our getter above to grab the list instead of getting it directly...this makes it easier on us if our data source changes...
-  renderLocationList(this.parentElement, this.getAllLocations());
-  this.addLocationListener();
+  renderHikeList(this.parentElement, this.getAllHikes());
+  this.addHikeListener();
   // make sure the back button is hidden
   this.backButton.classList.add('hidden');
 }
 // show one location with full details in the parentElement
-showOneLocation(locationName) {
-  const location = this.getLocationByName(locationName);
+showOneHike(locationName) {
+  const location = this.getHikeByName(locationName);
   this.parentElement.innerHTML = '';
-  this.parentElement.appendChild(renderOneLocationFull(location));
+  this.parentElement.appendChild(renderOneHikeFull(location));
   // show the back button
   this.backButton.classList.remove('hidden');
 }
 // in order to show the details of a location ontouchend we will need to attach a listener AFTER the list of locations has been built. The function below does that.
-addLocationListener() {
+addHikeListener() {
   // We need to loop through the children of our list and attach a listener to each, remember though that children is a nodeList...not an array. So in order to use something like a forEach we need to convert it to an array.
   const childrenArray = Array.from(this.parentElement.children);
   childrenArray.forEach(child => {
     child.addEventListener('touchend', e => {
       // why currentTarget instead of target?
-      this.showOneLocation(e.currentTarget.dataset.name);
+      this.showOneHike(e.currentTarget.dataset.name);
     });
   });
 }
 buildBackButton() {
   const backButton = document.createElement('button');
-  backButton.innerHTML = '&lt;- All Locations';
+  backButton.innerHTML = '&lt;- All Hikes';
   backButton.addEventListener('touchend', () => {
-    this.showLocationList();
+    this.showHikeList();
   });
   backButton.classList.add('hidden');
   this.parentElement.before(backButton);
   return backButton;
 }
 }
-// End of Locations class
+// End of Hikes class
 // methods responsible for building HTML.  Why aren't these in the class?  They don't really need to be, and by moving them outside of the exported class, they cannot be called outside the module...they become private.
-function renderLocationList(parent, locations) {
+function renderHikeList(parent, locations) {
 locations.forEach(location => {
-  parent.appendChild(renderOneLocationLight(location));
+  parent.appendChild(renderOneHikeLight(location));
 });
 }
 
-function renderOneLocationLight(location) {
+function renderOneHikeLight(location) {
 const item = document.createElement('li');
 item.classList.add('light');
 // setting this to make getting the details for a specific location easier later.
@@ -114,7 +114,7 @@ item.innerHTML = ` <h2>${location.name}</h2>
 return item;
 }
 
-function renderOneLocationFull(location) {
+function renderOneHikeFull(location) {
 const item = document.createElement('li');
 item.innerHTML = ` 
   
