4947d286723d7f98feca092c867a5291ddc2c3ab
JosephHark <71291466+JosephHark@users.noreply.github.com>
2021-03-02 21:33:12 -0500

    c

--- index/final/js/locations.js
+++ index/final/js/locations.js
@@ -2,141 +2,130 @@
 
 //create an array of locations
 const locationList = [{
-  name: 'Bechler Falls',
-  imgSrc: 'falls.jpg',
-  imgAlt: 'Image of Bechler Falls',
-  distance: '3 miles',
-  difficulty: 'Easy',
-  description: 'Beautiful short location along the Bechler river to Bechler Falls',
-  directions: 'Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead.'
-},
-{
-  name: 'Teton Canyon',
-  imgSrc: 'falls.jpg',
-  imgAlt: 'Image of Bechler Falls',
-  distance: '3 miles',
-  difficulty: 'Easy',
-  description: 'Beautiful short (or long) location through Teton Canyon.',
-  directions: 'Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Staline Raod for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead.'
-},
-{
-  name: 'Denanda Falls',
-  imgSrc: 'falls.jpg',
-  imgAlt: 'Image of Bechler Falls',
-  distance: '7 miles',
-  difficulty: 'Moderate',
-  description: 'Beautiful location through Bechler meadows river to Denanda Falls',
-  directions: 'Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to until you see the sign for Bechler Meadows on the left. Turn there. There is a parking area at the trailhead.'
-}
-];
-
-const imgBasePath = '//byui-cit.github.io/cit261/examples/';
+  name: 'a1',
+  state: 'a2',
+  fact: 'a3',
+  img: 'a4',
+  imgAlt: 'a5',
+  indoor: 'a6',
+  description: 'a7',
+  img2: 'a9',
+  imgAlt2: 'a10',
+  img3: 'a11',
+  imgAlt3: 'a12'
+}];
 
 export default class Hikes {
-constructor(elementId) {
-  this.parentElement = document.getElementById(elementId);
-  // we need a back button to return back to the list. This will build it and hide it. When we need it we just need to remove the 'hidden' class
-  this.backButton = this.buildBackButton();
-}
-// why is this function necessary?  locationList is not exported, and so it cannot be seen outside of this module. I added this in case I ever need the list of locations outside of the module. This also sets me up nicely if my data were to move. I can just change this method to the new source and everything will still work if I only access the data through this getter.
-getAllHikes() {
-  return locationList;
-}
-// For the first stretch we will need to get just one location.
-getHikeByName(locationName) {
-  return this.getAllHikes().find(location => location.name === locationName);
-}
-//show a list of locations in the parentElement
-showHikeList() {
-  this.parentElement.innerHTML = '';
-  // notice that we use our getter above to grab the list instead of getting it directly...this makes it easier on us if our data source changes...
-  renderHikeList(this.parentElement, this.getAllHikes());
-  this.addHikeListener();
-  // make sure the back button is hidden
-  this.backButton.classList.add('hidden');
-}
-// show one location with full details in the parentElement
-showOneHike(locationName) {
-  const location = this.getHikeByName(locationName);
-  this.parentElement.innerHTML = '';
-  this.parentElement.appendChild(renderOneHikeFull(location));
-  // show the back button
-  this.backButton.classList.remove('hidden');
-}
-// in order to show the details of a location ontouchend we will need to attach a listener AFTER the list of locations has been built. The function below does that.
-addHikeListener() {
-  // We need to loop through the children of our list and attach a listener to each, remember though that children is a nodeList...not an array. So in order to use something like a forEach we need to convert it to an array.
-  const childrenArray = Array.from(this.parentElement.children);
-  childrenArray.forEach(child => {
-    child.addEventListener('touchend', e => {
-      // why currentTarget instead of target?
-      this.showOneHike(e.currentTarget.dataset.name);
+  constructor(elementId) {
+    this.parentElement = document.getElementById(elementId);
+    // we need a back button to return back to the list. This will build it and hide it. When we need it we just need to remove the 'hidden' class
+    this.backButton = this.buildBackButton();
+  }
+  // why is this function necessary?  locationList is not exported, and so it cannot be seen outside of this module. I added this in case I ever need the list of locations outside of the module. This also sets me up nicely if my data were to move. I can just change this method to the new source and everything will still work if I only access the data through this getter.
+  getAllHikes() {
+    return locationList;
+  }
+  // For the first stretch we will need to get just one location.
+  getHikeByName(locationName) {
+    return this.getAllHikes().find(location => location.name === locationName);
+  }
+  //show a list of locations in the parentElement
+  showHikeList() {
+    this.parentElement.innerHTML = '';
+    // notice that we use our getter above to grab the list instead of getting it directly...this makes it easier on us if our data source changes...
+    renderHikeList(this.parentElement, this.getAllHikes());
+    this.addHikeListener();
+    // make sure the back button is hidden
+    this.backButton.classList.add('hidden');
+  }
+  // show one location with full details in the parentElement
+  showOneHike(locationName) {
+    const location = this.getHikeByName(locationName);
+    this.parentElement.innerHTML = '';
+    this.parentElement.appendChild(renderOneHikeFull(location));
+    // show the back button
+    this.backButton.classList.remove('hidden');
+  }
+  // in order to show the details of a location ontouchend we will need to attach a listener AFTER the list of locations has been built. The function below does that.
+  addHikeListener() {
+    // We need to loop through the children of our list and attach a listener to each, remember though that children is a nodeList...not an array. So in order to use something like a forEach we need to convert it to an array.
+    const childrenArray = Array.from(this.parentElement.children);
+    childrenArray.forEach(child => {
+      child.addEventListener('touchend', e => {
+        // why currentTarget instead of target?
+        this.showOneHike(e.currentTarget.dataset.name);
+      });
     });
-  });
-}
-buildBackButton() {
-  const backButton = document.createElement('button');
-  backButton.innerHTML = '&lt;- All Hikes';
-  backButton.addEventListener('touchend', () => {
-    this.showHikeList();
-  });
-  backButton.classList.add('hidden');
-  this.parentElement.before(backButton);
-  return backButton;
-}
+  }
+  buildBackButton() {
+    const backButton = document.createElement('button');
+    backButton.innerHTML = '&lt;- All Hikes';
+    backButton.addEventListener('click', () => {
+      this.showHikeList();
+    });
+    backButton.classList.add('hidden');
+    this.parentElement.before(backButton);
+    return backButton;
+  }
 }
 // End of Hikes class
 // methods responsible for building HTML.  Why aren't these in the class?  They don't really need to be, and by moving them outside of the exported class, they cannot be called outside the module...they become private.
 function renderHikeList(parent, locations) {
-locations.forEach(location => {
-  parent.appendChild(renderOneHikeLight(location));
-});
+  locations.forEach(location => {
+    parent.appendChild(renderOneHikeLight(location));
+  });
 }
 
 function renderOneHikeLight(location) {
-const item = document.createElement('li');
-item.classList.add('light');
-// setting this to make getting the details for a specific location easier later.
-item.setAttribute('data-name', location.name);
-item.innerHTML = ` <h2>${location.name}</h2>
-<div class="image"><img src="${imgBasePath}${location.imgSrc}" alt="${location.imgAlt}"></div>
+  const item = document.createElement('ol');
+  item.classList.add('light');
+  // setting this to make getting the details for a specific location easier later.
+  item.setAttribute('data-name', location.name);
+  item.setAttribute('data-state', location.state);
+  item.innerHTML =
+` <h2>${location.name}</h2>
+ <h2>${location.state}</h2>
+<div class="image"><img src="${location.imgSrc}" alt="${location.imgAlt}"></div>
 <div>
       <div>
-          <h3>Distance</h3>
-          <p>${location.distance}</p>
+          <h3>Fun Fact</h3>
+          <p>${location.fact}</p>
       </div>
       <div>
-          <h3>Difficulty</h3>
-          <p>${location.difficulty}</p>
+          <h3>Is this in indoor or outside?</h3>
+          <p>This is ${location.indoor}</p>
       </div>
 </div>`;
 
-return item;
+  return item;
 }
 
 function renderOneHikeFull(location) {
-const item = document.createElement('li');
-item.innerHTML = ` 
+  const item = document.createElement('ol');
+  item.innerHTML = ` 
   
-      <img src="${imgBasePath}${location.imgSrc}" alt="${location.imgAlt}">
-      <h2>${location.name}</h2>
-      <div>
-          <h3>Distance</h3>
-          <p>${location.distance}</p>
-      </div>
-      <div>
-          <h3>Difficulty</h3>
-          <p>${location.difficulty}</p>
-      </div>
-      <div>
+  
+ <div class="image"><img src="${location.img}" alt="${location.imgAlt}"></div>
+ <div>
+ <h2>${location.name}</h2>
+  <h2>${location.state}</h2>
+       <div>
+           <h3>Distance</h3>
+           <p>${location.fact}</p>
+       </div>
+       <div>
+           <h3>Is this indoor or outside?</h3>
+           <p>This is ${location.indoor}</p>
+       </div>
           <h3>Description</h3>
           <p>${location.description}</p>
       </div>
       <div>
-          <h3>How to get there</h3>
-          <p>${location.directions}</p>
-      </div>
+          <h3>More Pictures</h3>
+          <div class="image"><img src="${location.img2}" alt="${location.img2}"></div>
+          <div class="image"><img src="${location.img3}" alt="${location.img3}"></div>
+          </div>
   
   `;
-return item;
+  return item;
 }
